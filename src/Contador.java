242a2faeddb9957bba50b7f9b10074ca838407e7
Victoria Correia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-06-08 20:26:57 -0300

    Update Contador.java

--- src/Contador.java
+++ src/Contador.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Contador {
+public class contadorador {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
         int parametro1, parametro2;
@@ -11,24 +11,23 @@ public class Contador {
 		parametro2 = scan.nextInt();
 		
         scan.close();
-
 		try {
-			contar(parametro1, parametro2);
+			contadorar(parametro1, parametro2);
 		
 		}catch (ParametrosInvalidosException exception) {
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
 		}
 	}
-	static void contar(int parametro1, int parametro2) throws ParametrosInvalidosException {
-        int cont;
+	static void contadorar(int parametro1, int parametro2) throws ParametrosInvalidosException {
+        int contador;
         if (parametro1 > parametro2)
             throw new ParametrosInvalidosException();
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
 		else
-		    cont = parametro2 - parametro1;
-            for(int i = 1; i <= cont; i++){
+		    contador = parametro2 - parametro1;
+            for(int i = 1; i <= contador; i++){
                 System.out.println("Imprimindo o número " + i);
             }
-		//realizar o for para imprimir os números com base na variável contagem
+		//realizar o for para imprimir os números com base na variável contadoragem
 	}
 }
